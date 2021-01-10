4e611b9ecec7c8e2d622e6022017bbfeecd43ddd
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-01-10 10:11:16 -0800

    style changes

--- src/components/VideoDetail/index.js
+++ src/components/VideoDetail/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const VideoBase = styled.div`
-    
-    .videoPlayer {
-        max-width: 720px;
-    }
-
-    @media (max-width: 800px) {
-        .videoPlayer {
-            align-items: center;
-            max-width: 340px;
-            margin: 0 10px 0 10px;
-        }
+    width: 720px;
+    @media (max-width: 800px) {        
+        width: 320px;               
     }
 `;
 
@@ -26,7 +18,7 @@ const VideoDetail = ({video}) => {
     return(
         <VideoBase >
             <div className="ui embed">
-                <iframe className="videoPlayer" src={videoSrc} title="Video Player"/>
+                <iframe src={videoSrc} title="Video Player"/>
             </div>
             <div className="ui segment">
                 <h4 className="ui header">
